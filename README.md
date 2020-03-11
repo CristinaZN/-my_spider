57a2cc72ffa06c228ce9254319b39f07e771d9ff
CristinaZN <710859414@qq.com>
2020-03-11 23:39:47 +0800

    update spider_bilibili

---
+++ README.md
@@ -0,0 +1,54 @@
+# [Spider for [bilibili](www.bilibili.com)]
+
+This spider is **only** **for research** **in personal** to gather information of finished animes. 
+
+It can get those details of finished animes,
+
+```python
+['title', 'views', 'bullet_comment', 'fans_num','score', 'tags', 'seiyuu', 'av_num']
+```
+
+and form a data set called 'bilibili_fin_anime.csv'
+
+### Get the spider
+
+```
+$ git clone 
+```
+
+### Environment setup
+
+##### Libraries:
+
+- ​	selenium
+- ​	BeautifulSoup
+- ​	lxml
+
+```
+pip3 install selenium bs4 lxml
+```
+
+##### Browser: Chrome + chromedriver
+
+### Instruction
+
+```
+python3 spider.py -s <start_page> -e <end_page>
+```
+
+### TO DO
+
+- finish  functions:
+
+  ```python
+  def download_cover_img(url)
+  ```
+
+  The spider cannot gather the cover image, because [bilibili](www.bilibili.com) optimize its page by jQuery.lazyload.
+
+  ```python
+  def gen_json(charactor,seiyuu)
+  ```
+
+  The charactors and the seiyuu of a anime is now saved as strings in the csv file. 
+
---
+++ spider.py
@@ -0,0 +1,231 @@
+import bs4
+import csv
+import re
+import sys
+import getopt
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+def main(argv):
+    # default args
+    start_page = -1
+    end_page = -1
+    try:
+        opts, args = getopt.getopt(argv, "hs:e:", ["s_page=", "e_page="])
+    except:
+        print('[Error]: lack of parameter or invalid argument, please use \'-h\' for help')
+        exit()
+    for opt, arg in opts:
+        if opt == '-h':
+            print('[Usage]: spider.py -s <start_page>(int) -e <end_page>(int)')
+            exit()
+        elif opt in ("-s", "--s_page"):
+            if int(arg) < 1 or int(arg) > 700:
+                print('[Error]: start page cannot be lower than 1')
+                sys.exit()
+            start_page = arg
+        elif opt in ("-e", "--e_page"):
+            if int(arg) > 700:
+                print('[Error]: end page cannot be greater than 700')
+                sys.exit()
+            if int(arg) < 0:
+                print('[Error]: end page cannot be lower than 1')
+                sys.exit()
+            end_page = arg
+        else:
+            print('[Error]: lack of parameter or invalid argument, please use \'-h\' for help')
+            exit()
+    if start_page == -1 or end_page == -1:
+        print('[Error]: lack of parameter or invalid argument, please use \'-h\' for help')
+        exit()
+    fin_anime = [[], [], [], [], [], []]
+    #   fin_anime[0] = title,
+    #   fin_anime[1] = param,   #   param:  ['views_num','bullet_comment_num','fans_num','score']
+    #   fin_anime[2] = tag,     #   tag:    ['tag1',...,'tag_n']
+    #   fin_anime[3] = seiyuu.  #   seiyuu: ['json1',...,'json_n']
+    #   fin_anime[4] = av_num
+
+    # 20 anime per page
+    try:
+        for page in range(start_page-1, end_page):
+            print('get page:' + str(page + 1))
+            one_page = get_fin_anime(page)
+            for title in one_page[0]:
+                fin_anime[0].append(title)
+            for param in one_page[1]:
+                fin_anime[1].append(param)
+            for tag in one_page[2]:
+                fin_anime[2].append(tag)
+            for seiyuu in one_page[3]:
+                fin_anime[3].append(seiyuu)
+            for av_num in one_page[4]:
+                fin_anime[4].append(av_num)
+            # for img_url in one_page[5]:
+            #     fin_anime[5].append(img_url)
+    except:
+        print("[Error]: lack of parameter or invalid argument, please use \'-h\' for help")
+        exit()
+    # form csv
+    form_csv(fin_anime)
+
+
+def form_csv(fin_anime):
+    headers = ['title', 'views', 'bullet_comment', 'fans',
+               'score', 'tags', 'seiyuu', 'av_num']
+    data = [[], [], [], [], [], [], [], []]
+    for title in fin_anime[0]:
+        data[0].append(title)
+    for param in fin_anime[1]:
+        data[1].append(param[0])
+        data[2].append(param[1])
+        if len(param) == 2:
+            data[3].append('null')
+            data[4].append('null')
+        else:
+            data[3].append(param[2])
+            data[4].append(param[3])
+    for tag in fin_anime[2]:
+        data[5].append(tag)
+    for seiyuu in fin_anime[3]:
+        data[6].append(seiyuu)
+    for av_num in fin_anime[4]:
+        data[7].append(av_num)
+
+    rows = []
+    for i in range(len(data[0])):
+        rows.append([data[0][i], data[1][i], data[2][i], data[3][i], data[4][i]
+                        ,data[5][i], data[6][i], data[7][i]])
+    with open('bilibili_fin_anime.csv', 'w', encoding='utf-8') as f:
+        writer = csv.writer(f)
+        writer.writerow(headers)
+        writer.writerows(rows)
+
+    #   download cover
+    # for anime in rows:
+    #     img_url = 'https://' + anime[7]
+    #     with open('./cover_img/' + anime[0] + '.jpg', 'wb') as f_img:
+    #         f_img.write(requests.get(img_url).content)
+
+
+def get_fin_anime(page):
+    chrome_options = Options()
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('blink-settings=imagesEnabled=false')
+    # browser = webdriver.Chrome(chrome_options=chrome_options)
+    anime_num = 0
+    fin_anime_list = []
+    href_list = []
+    title_list = []
+    param_set = []  # element in param_set: ['views_num','bullet_comment_num','fans_num','score']
+    seiyuu_set = []
+    av_num_list = []
+    img_url_list = []
+    view = ''
+    url = 'https://www.bilibili.com/v/anime/finish/#/all/default/0/' + str(page + 1) + '/'
+
+    #   get href, title, av_num of every anime
+    browser = webdriver.Chrome(options=chrome_options)
+    browser.get(url)
+    # for j in range(1, 8):
+    #     js = "var q=document.documentElement.scrollTop=" + str(500 * j)
+    #     browser.execute_script(js)
+    #     time.sleep(3)
+    soup = bs4.BeautifulSoup(browser.page_source, 'lxml')
+    div_list = soup.find_all('div', class_='r')
+    v_info_list = soup.find_all('span', class_="v-info-i")
+    titles = soup.find_all('a', class_='title')
+    av_No = soup.find_all('div', class_='l-item')
+    print(av_No, len(av_No))
+    href_re = re.compile(r'www.bilibili.com/video/av\d*')
+    av_re = re.compile(r'av\d*')
+    title_re = re.compile(r'>.*')
+    v_info_re = re.compile(r'<span>.*')
+    # img_re = re.compile(r'src="[\w\d.@]*')
+    for div in div_list:
+        href = href_re.findall(str(div))
+        href_list.append(href[0])
+    for item in titles:
+        name = title_re.findall(str(item))[0]
+        title_list.append(str(name).lstrip('>').rstrip('</a>'))
+    for av in av_No:
+        num = av_re.findall(str(av))[0]
+        # img_src = img_re.findall(str(av))
+        # img_url_list.append(str(img_src[0]).lstrip('src="').rstrip('"'))
+        av_num_list.append(num)
+    for i in range(len(v_info_list)):
+        if i % 2 == 0:
+            view = v_info_re.findall(str(v_info_list[i]))
+        else:
+            comment = v_info_re.findall(str(v_info_list[i]))
+            param_set.append([str(view).lstrip('\'[<span><div>').rstrip('</span></div>\']'),
+                              str(comment).lstrip('\'[<span><div>').rstrip('</span></div>\']')])
+    browser.close()
+    print('get titles')
+    print(title_list)
+
+    #   get intro page of every anime
+    print('waiting for getting intro_page urls, about 4 min based on your network')
+    href_counter = 0
+    intro_page_list = []
+    for href_item in href_list:
+        browser = webdriver.Chrome(options=chrome_options)
+        browser.get('https://' + href_item)
+        time.sleep(10)
+        browser.get(browser.current_url)
+        href_counter += 1
+        print('get intro_page urls ' + str(href_counter) + '/20: ' + browser.current_url)
+        intro_soup = bs4.BeautifulSoup(browser.page_source, 'lxml')
+        intro_div_list = intro_soup.find_all('div', class_='media-info clearfix report-wrap-module')
+        intro_href_re = re.compile(r'www.bilibili.com/bangumi/media/md\d*/')
+        for div in intro_div_list:
+            intro_page = intro_href_re.findall(str(div))
+            if len(intro_page) >= 1:
+                intro_page_list.append(intro_page[0])
+            else:
+                intro_page_list.append("Null")
+        browser.close()
+
+    #   get tags, scores, fans_num, seiyuu of every anime
+    print('waiting for accessing intro_pages:' + str(anime_num + 1) + '/20')
+    tag_set = []
+    for intro_page in intro_page_list:
+        if intro_page != "Null":
+            browser = webdriver.Chrome(options=chrome_options)
+            browser.get('http://' + intro_page)
+            detail_soup = bs4.BeautifulSoup(browser.page_source, 'lxml')
+            tags = detail_soup.find_all('span', class_='media-tag')
+            if len(detail_soup.find_all('div', class_='media-info-score-content')) == 0:
+                score = '<div class="media-info-score-content"> Null </div>'
+            else:
+                score = detail_soup.find_all('div', class_='media-info-score-content')[0]
+            fans = detail_soup.find_all('em')[1]
+            seiyuu = str(detail_soup.find_all('span', style='opacity: 0;')[0]). \
+                lstrip(' <span class="hide" style="opacity: 0;">').rstrip('</p></span>')
+            print("get Seiyuus" + str(anime_num + 1) + '/20')
+            print(seiyuu)
+            tag_list = []
+            for tag in tags:
+                tag_list.append(str(tag)[24:len(tag) - 8])
+            tag_set.append(tag_list)
+            param_set[anime_num].append(str(fans).lstrip('<em>').rstrip('</em>'))
+            param_set[anime_num].append(str(score).lstrip('<div class="media-info-score-content">').rstrip('</div>'))
+            anime_num += 1
+            seiyuu_set.append(seiyuu)
+        else:
+            tag_set.append(['Null'])
+            param_set[anime_num].append('Null')
+            param_set[anime_num].append('Null')
+            seiyuu_set.append('Null')
+            anime_num += 1
+    print("get param:  ['views_num','bullet_comment_num','fans_num','score']")
+    print(param_set)
+    print("get_tags")
+    print(tag_set)
+    return [title_list, param_set, tag_set, seiyuu_set, av_num_list]
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
